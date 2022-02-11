2d7f3607ed45c3d34da547f02d6084c0d047a43a
marianphillips <79763160+marianphillips@users.noreply.github.com>
2022-02-11 17:08:53 +0000

    completed task but needs refactoring

--- index.js
+++ index.js
@@ -9,55 +9,55 @@ const state = {
     {
       id: "002-carrot",
       name: "carrot",
-      price: 0.35,
+      price: 0.11,
       type: "veg"
     },
     {
       id: "003-apple",
       name: "apple",
-      price: 0.35,
+      price: 0.40,
       type: "fruit"
     },
     {
       id: "004-apricot",
       name: "apricot",
-      price: 0.35,
+      price: 0.71,
       type: "fruit"
     },
     {
       id: "005-avocado",
       name: "avocado",
-      price: 0.35,
+      price: 0.90,
       type: "veg"
     },
     {
       id: "006-bananas",
       name: "bananas",
-      price: 0.35,
+      price: 0.23,
       type: "fruit"
     },
     {
       id: "007-bell-pepper",
       name: "bell pepper",
-      price: 0.35,
+      price: 0.45,
       type: "veg"
     },
     {
       id: "008-berry",
       name: "berry",
-      price: 0.35,
+      price: 0.05,
       type: "fruit"
     },
     {
       id: "009-blueberry",
       name: "blueberry",
-      price: 0.35,
+      price: 0.05,
       type: "fruit"
     },
     {
       id: "010-eggplant",
       name: "eggplant",
-      price: 0.35,
+      price: 0.65,
       type: "veg"
     }
   ],
@@ -73,6 +73,8 @@ const cartItemList = document.querySelector('.cart--item-list')
 const total = document.querySelector('.total-number')
 const fruit = document.querySelector('#fruit')
 const veg = document.querySelector('#veg')
+const sort = document.querySelector('#sort-items')
+
 
 fruit.checked = true
 veg.checked= true
@@ -87,6 +89,26 @@ veg.addEventListener('change', function() {
   renderStoreItems ()
 })
 
+sort.addEventListener('change', function(event) {
+  if(event.target.value === "most-popular") {
+    state.items.sort((a, b) => a.id.localeCompare(b.id))
+  }
+
+  if(event.target.value === "lowest-price") {
+    state.items.sort((a,b) => a.price - b.price)
+ }
+
+ if(event.target.value === "highest-price") {
+  state.items.sort((a,b) => b.price - a.price)
+}
+
+if(event.target.value === "alphabetically") {
+  state.items.sort((a, b) => a.name.localeCompare(b.name))
+}
+
+renderStoreItems()
+
+})
 
 function renderStoreItems() {
 storeItems.innerHTML = ""
