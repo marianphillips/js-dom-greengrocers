58b446b7f6737512aa33fb1d63dbf9a6eebd407c
marianphillips <79763160+marianphillips@users.noreply.github.com>
2022-02-11 15:40:20 +0000

    core criteria completed

--- index.js
+++ index.js
@@ -53,3 +53,102 @@ const state = {
   ],
   cart: []
 };
+
+
+//make list of store items
+const storeItems = document.querySelector('.store--item-list')
+const cartItemList = document.querySelector('.cart--item-list')
+const total = document.querySelector('.total-number')
+
+
+function renderStoreItems() {
+for(const item of state.items) {
+let listItemInStore = document.createElement('li')
+listItemInStore.innerHTML = `<div class="store--item-icon">
+<img src="assets/icons/${item.id}.svg" alt="${item.name}" />
+</div>
+<button>Add to cart</button>`
+storeItems.append(listItemInStore)
+
+//make buttons add to cart
+const button = listItemInStore.querySelector('button')
+button.addEventListener('click', function() {
+if(state.cart.filter(x => x.name === item.name).length === 1) {
+    for(let itemInCart of state.cart){
+      if(itemInCart.name === item.name) {
+        itemInCart.quantity++
+      }
+    }
+  }
+  else{
+    const cartItem = {
+      id: item.id,
+      name: item.name,
+      price: item.price,
+      quantity: 1
+    }
+ 
+    state.cart.push(cartItem) 
+  }
+    renderCartItems()
+    renderTotal()
+})
+}
+}
+
+
+
+
+function renderCartItems() {
+cartItemList.innerHTML = ""
+
+  for(let itemInCart of state.cart){
+  let cartItem = document.createElement('li')
+  cartItem.innerHTML = `<img
+  class="cart--item-icon"
+  src="assets/icons/${itemInCart.id}.svg"
+  alt="${itemInCart.name}"
+/>
+<p>${itemInCart.name}</p>
+<button class="quantity-btn remove-btn center">-</button>
+<span class="quantity-text center">${itemInCart.quantity}</span>
+<button class="quantity-btn add-btn center">+</button>`
+cartItemList.append(cartItem)
+
+const addButton = cartItem.querySelector('.add-btn')
+const removeButton = cartItem.querySelector('.remove-btn')
+
+addButton.addEventListener('click', function() {
+          itemInCart.quantity++
+      renderCartItems()
+      renderTotal()
+  })
+
+removeButton.addEventListener('click', function() {
+    itemInCart.quantity--
+    if(itemInCart.quantity === 0) {
+      state.cart.splice(state.cart.indexOf(itemInCart),1)
+    }   
+renderCartItems()
+renderTotal()
+})
+
+}
+
+}
+
+
+function renderTotal() {
+  let totalNumber = 0
+ for(let itemInCart of state.cart){
+  totalNumber += (itemInCart.quantity * itemInCart.price)
+ }
+  total.innerText = `£${totalNumber.toFixed(2)}`
+}
+
+
+
+renderStoreItems()
+
+
+
